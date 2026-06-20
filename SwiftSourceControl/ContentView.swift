b8d1955bbe303cf2932fe28b744b3161f2832403
Surya Sai Gopalam <suryasaigopalam@gmail.com>
2026-06-20 19:19:14 +0530

    Initial Commit

---
+++ SwiftSourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSourceControl
+//
+//  Created by Surya Sai Gopalam on 20/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSourceControl/SwiftSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlApp.swift
+//  SwiftSourceControl
+//
+//  Created by Surya Sai Gopalam on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
