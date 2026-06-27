76178787b9a310052457d76dbe6e28ae6bf2071c
Surya Sai Gopalam <suryasaigopalam@gmail.com>
2026-06-27 16:46:46 +0530

    HomeVIew Finished

--- SwiftSourceControl/HomeView.swift
+++ SwiftSourceControl/HomeView.swift
@@ -15,6 +15,13 @@ struct HomeView: View {
             Text("Screen 2")
 
         }
+        
+        VStack {
+            Text("Screen 2")
+            Text("Screen 2")
+            Text("Screen 2")
+
+        }
     }
 }
 
