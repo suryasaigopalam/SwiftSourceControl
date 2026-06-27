76940ec9d42bf0986738f68ee46ae5a18f56df51
Surya Sai Gopalam <suryasaigopalam@gmail.com>
2026-06-27 16:43:21 +0530

    Add HomeView

---
+++ SwiftSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftSourceControl
+//
+//  Created by Surya Sai Gopalam on 27/06/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
