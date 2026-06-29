a20e38be65fdcad686a9063357eb13bc387692f7
Surya Sai Gopalam <suryasaigopalam@gmail.com>
2026-06-29 16:41:25 +0530

    Great for committing

--- SwiftSourceControl/HomeView.swift
+++ SwiftSourceControl/HomeView.swift
@@ -10,38 +10,28 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         ZStack {
-            HStack {
+            
+            
+            VStack {
                 Text("Screen 2")
                 Text("Screen 2")
                 Text("Screen 2")
-
+                
             }
-            Text("Screen 2")
-            Text("Screen 2")
-            Text("Screen 2")
-
+            VStack {
+                Text("Screen 2")
+                Text("Screen 2")
+                Text("Screen 2")
+                
+            }
+            VStack {
+                Text("Screen 2")
+                Text("Screen 2")
+                Text("Screen 2")
+                
+            }
+            
         }
-        
-        VStack {
-            Text("Screen 2")
-            Text("Screen 2")
-            Text("Screen 2")
-
-        }
-        VStack {
-            Text("Screen 2")
-            Text("Screen 2")
-            Text("Screen 2")
-
-        }
-        VStack {
-            Text("Screen 2")
-            Text("Screen 2")
-            Text("Screen 2")
-
-        }
-        
-       
     }
 }
 
